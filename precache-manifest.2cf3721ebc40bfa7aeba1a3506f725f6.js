c46d5f5835ba8d2daec305c2281fd9e3474cd087
Sima <sima.amini@outlook.com>
2019-11-24 20:39:18 +0330

    Updates

---
+++ precache-manifest.2cf3721ebc40bfa7aeba1a3506f725f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0e6d5e6bd22b3b2c9139ba40fb0bc1",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "908174ceeb8c6c3b1fc1",
+    "url": "/react-todo-app/static/css/main.ec654f4e.chunk.css"
+  },
+  {
+    "revision": "0c6e9309c098b25928bb",
+    "url": "/react-todo-app/static/js/2.a3d6a181.chunk.js"
+  },
+  {
+    "revision": "908174ceeb8c6c3b1fc1",
+    "url": "/react-todo-app/static/js/main.6a0cfd4d.chunk.js"
+  },
+  {
+    "revision": "058617ebb7442edaa5e3",
+    "url": "/react-todo-app/static/js/runtime-main.2b4dc46c.js"
+  }
+]);
